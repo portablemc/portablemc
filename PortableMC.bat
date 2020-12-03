ba00d907946c029f4e04bfa3bf93c8c078679e14
yeetedmeme <58270183+yeetedmeme@users.noreply.github.com>
2020-12-03 13:35:14 -0500

    added java edtion and changed to local hosting for server.dat

--- PortableMC.bat
+++ PortableMC.bat
@@ -13,6 +13,7 @@ echo.
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 echo Portable Minecraft Launcher by yeeted_memezz
 echo Originaly Created By: LeeTheENTP
+echo Project Website: portablemc.github.io
 echo Please Check Out My Website yeetedmeme.js.org
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 if not exist "%CD%\GameData\AppData\.minecraft" mkdir "%CD%\GameData\AppData\.minecraft"
@@ -32,7 +33,7 @@ color 0c
 echo Minecraft launcher not found! Downloading...
 timeout /t 3 /nobreak > NUL
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://launcher.mojang.com/download/Minecraft.exe' '%CD%\GameData\Minecraft\Minecraft.exe'"
-powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://cdn.glitch.com/8b83ab14-1f56-4493-b084-12b79c406fc2%2Fservers.dat?v=1590542981798' '%CD%\GameData\AppData\.minecraft\servers.dat'"
+powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://yeetedmeme.js.org/file-hosting/servers.dat' '%CD%\GameData\AppData\.minecraft\servers.dat'"
 goto launch
  
 :end
