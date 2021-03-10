e28df3a0bbd221cdf16d6406bde236b41b655e64
Ashvin Henry <58270183+yeetedmeme@users.noreply.github.com>
2021-03-10 13:14:24 -0500

    fixed a fatal error

--- PortableMC.bat
+++ PortableMC.bat
@@ -15,7 +15,7 @@ echo Portable Minecraft Launcher by yeeted_memezz
 echo Originaly Created By: LeeTheENTP
 echo Project Website: portablemc.github.io
 echo Please Check Out My Website yeetedmeme.js.org
-echo Thanks Everyone For 1k Downloads!
+echo v1.3
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 if not exist "%CD%\GameData\AppData\.minecraft" mkdir "%CD%\GameData\AppData\.minecraft"
 if not exist "%CD%\GameData\Minecraft" mkdir "%CD%\GameData\Minecraft"
@@ -34,7 +34,7 @@ color 0c
 echo Minecraft launcher not found! Downloading...
 timeout /t 3 /nobreak > NUL
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://launcher.mojang.com/download/Minecraft.exe' '%CD%\GameData\Minecraft\Minecraft.exe'"
-powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://yeetedmeme.js.org/file-hosting/servers.dat' '%CD%\GameData\AppData\.minecraft\servers.dat'"
+powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://www.dropbox.com/s/ly1111w198fnvt0/servers.dat?dl=1' '%CD%\GameData\AppData\.minecraft\servers.dat'"
 goto launch
  
 :end
