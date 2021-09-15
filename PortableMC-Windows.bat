a0f8f5c5cf9f0bb6adf69bd85d78ad5d2b98d48a
greymesa <58270183+greymesa@users.noreply.github.com>
2021-09-15 13:45:14 -0400

    Update PortableMC-Windows.bat

--- PortableMC-Windows.bat
+++ PortableMC-Windows.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBpYTQqRAE+1EbsQ5+n//NaLrUoSGus8d+8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSTk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZkgaH2Q=
-::ZQ05rAF9IBncCkqN+0xwdVsFAlfMbws=
-::ZQ05rAF9IAHYFVzEqQIAJwlATQaOOUaZZg==
-::eg0/rx1wNQPfEVWB+kM9LVsJDDSNLn+7BLkZxcu22OOJpkIKR4I=
-::fBEirQZwNQPfEVWB+kM9LVsJDDSNLn+7BLkZxcu22OOJpkIKR4I=
-::cRolqwZ3JBvQF1fEqQIAJwlATQaOOSuXD7sZ6/r66f7HiEwLVaIafIbP07uAYYA=
-::dhA7uBVwLU+EWEmL5lYxKhdRQQfi
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBpYTQqRAE+1EbsQ5+n//NaXrV8JVeAzfaL4l4OHLuQc/VaqcI4otg==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 echo.
 echo.
