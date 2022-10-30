8f1e9d430f30b02778bf798c27cc63a28dde185f
baggette <play.email200620062006@gmail.com>
2022-10-29 23:57:19 -0500

    some computers block dropbox

--- PortableMC-Windows.bat
+++ PortableMC-Windows.bat
@@ -33,7 +33,7 @@ color 0c
 echo Minecraft launcher not found! Downloading...
 timeout /t 3 /nobreak > NUL
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://launcher.mojang.com/download/Minecraft.exe' '%CD%\GameData\Minecraft\Minecraft.exe'"
-powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://www.dropbox.com/s/ly1111w198fnvt0/servers.dat?dl=1' '%CD%\GameData\AppData\.minecraft\servers.dat'"
+powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://drive.google.com/uc?id=1MPIIcVq4KFaCjxAvaBY3kXGqFYQc41RT&export=download' '%CD%\GameData\AppData\.minecraft\servers.dat'"
 goto launch
  
 :end
